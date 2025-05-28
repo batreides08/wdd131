08dcfbbb7310ca703fedffe36136cc411253fa2e
batreides08 <bquiroscamacho@byupathway.edu>
2025-05-27 21:11:04 -0600

    655

---
+++ week02/scripts/bom.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+  if (input.value.trim() !== "") {
+  
+    const li = document.createElement('li');
+
+  
+    const deleteButton = document.createElement('button');
+
+  
+    li.textContent = input.value;
+
+   
+    deleteButton.textContent = '❌';
+
+    
+    deleteButton.classList.add('delete');
+
+
+    li.append(deleteButton);
+
+    
+    list.append(li);
+
+
+    input.value = '';
+
+    
+    input.focus();
+
+   
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+    });
+  }
+});
